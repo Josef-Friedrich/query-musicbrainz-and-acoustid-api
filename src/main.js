697d751dac246ae9459c3f6c831bc62d2295c5aa
Josef Friedrich <josef@friedrich.rocks>
2023-07-22 22:06:32 +0200

    Merge

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 #! /usr/bin/env node
 
-const URLSearchParams = require('url').URLSearchParams
+// fingerprint
+// https://acoustid.org/fingerprint/48005565
 
+// track ID: a cluster of fingerprints.
+// https://acoustid.org/track/47b1aca5-5ec9-41a4-93c7-ceadad334087
+// 47b1aca5-5ec9-41a4-93c7-ceadad334087
+
+const childProcess = require('child_process')
+const URLSearchParams = require('url').URLSearchParams
 const MusicBrainzApi = require('musicbrainz-api').MusicBrainzApi
+
+function openUrl (url) {
+  // See node module on npmjs.org “open”
+  const subprocess = childProcess.spawn('xdg-open', [url], {
+    stdio: 'ignore',
+    detached: true
+  })
+  subprocess.unref()
+}
 
 const mbApi = new MusicBrainzApi({
   appName: 'queryReleases',
@@ -10,10 +26,100 @@ const mbApi = new MusicBrainzApi({
   appContactInfo: 'josef@friedrich.rocks'
 })
 
-// release
-// https://musicbrainz.org/release/70d0009e-3b8d-4e03-ab41-2beb34a2c546
+async function query (url, query) {
+  const response = await fetch(url + '?' + new URLSearchParams(query))
+  const result = await response.json()
+  return result
+}
 
-async function getRecordingIdsByRelease (releaseId) {
+/**
+ * ```js
+ * {
+ *   results: [
+ *     {
+ *       id: 'be89452d-604d-41f4-bef4-1ec39ed5e25f',
+ *       recordings: [Array],
+ *       score: 1
+ *     }
+ *   ],
+ *   status: 'ok'
+ * }
+ * ```
+ *
+ * `result.results[0].recordings[0]`
+ *
+ * ```js
+ * {
+ *   artists: [
+ *     {
+ *       id: 'da5e774b-026a-4117-82a4-11d246c05a8b',
+ *       name: 'György Ligeti'
+ *     }
+ *   ],
+ *   duration: 230,
+ *   id: 'fec2b24c-ed42-4025-b67d-6b5bba6565e9',
+ *   title: 'Artikulation'
+ * }
+ * ```
+ *
+ * or:
+ *
+ * `result.results[0].recordings[0]`
+ *
+ * ```js
+ * {
+ *   id: 'fec2b24c-ed42-4025-b67d-6b5bba6565e9'
+ * }
+ * ```
+ *
+ * @param {*} trackId
+ */
+async function listRecordingIdsByTrackId (trackId) {
+  const result = await query('https://api.acoustid.org/v2/lookup', {
+    client: 'Gp4IuWk1QAQ',
+    trackid: trackId,
+    meta: 'recordings'
+  })
+  if (result != null) {
+    return result.results[0].recordings.map(recording => {
+      recording.id
+    })
+  }
+}
+
+/**
+ * ```js
+ * {
+ *   status: 'ok',
+ *   tracks: [ { id: '096d3924-58da-450b-8ed8-7e5a68213fac' } ]
+ * }
+ * ```
+ *
+ * @param {string} recordingId - For example `70d0009e-3b8d-4e03-ab41-2beb34a2c546`
+ *
+ * @returns {Promise<string[]>}
+ */
+async function listTrackIdsByRecordingId (recordingId) {
+  const result = await query('https://api.acoustid.org/v2/track/list_by_mbid', {
+    mbid: recordingId
+  })
+  if (result != null && result.tracks != null) {
+    return result.tracks.map(track => {
+      return track.id
+    })
+  }
+  return []
+}
+
+/**
+ *
+ * https://musicbrainz.org/release/70d0009e-3b8d-4e03-ab41-2beb34a2c546
+ *
+ * @param {string} releaseId - For example `70d0009e-3b8d-4e03-ab41-2beb34a2c546`
+ *
+ * @returns {Promise<string[]>}
+ */
+async function listRecordingIdsByReleaseId (releaseId) {
   const release = await mbApi.lookupRelease(releaseId, ['recordings'])
   const recordingIds = []
   for (const cd of release.media) {
@@ -24,47 +130,32 @@ async function getRecordingIdsByRelease (releaseId) {
   return recordingIds
 }
 
-getRecordingIdsByRelease('70d0009e-3b8d-4e03-ab41-2beb34a2c546').then((recordingIds) => {
-  console.log(recordingIds)
-})
-
-// mbid: MusicBrainz Recording ID
-// 36c668ac-3cfb-44d9-a8e6-0a1d65a77a76
-
-// track ID: a cluster of fingerprints.
-// https://acoustid.org/track/47b1aca5-5ec9-41a4-93c7-ceadad334087
-// 47b1aca5-5ec9-41a4-93c7-ceadad334087
-
-// fingerprint
-// https://acoustid.org/fingerprint/48005565
-
-async function query (url, query) {
-  const response = await fetch(url + '?' + new URLSearchParams(query))
-  const result = await response.json()
-  console.log(result)
-  return result
-}
-
-async function listAcoustIdTracksByMbid (mbid) {
-  await query('https://api.acoustid.org/v2/track/list_by_mbid', {
-    mbid
-  })
-}
-
-listAcoustIdTracksByMbid('36c668ac-3cfb-44d9-a8e6-0a1d65a77a76')
-
-async function lookupAcoustIdByTrackId (trackid) {
-  const result = await query('https://api.acoustid.org/v2/lookup', {
-    client: 'Gp4IuWk1QAQ',
-    trackid,
-    meta: 'recordings'
-  })
-
-  for (const track of result.results) {
-    for (const recording of track.recordings) {
-      console.log(recording)
+/**
+ * @param {string} releaseId - For example `70d0009e-3b8d-4e03-ab41-2beb34a2c546`
+ *
+ * @returns {Promise<string[]>}
+ */
+async function openAcoustIdWithMultipleRecordings (releaseId) {
+  const recordingIds = await listRecordingIdsByReleaseId(releaseId)
+  const intervalId = setInterval(async () => {
+    const recordingId = recordingIds.pop()
+    if (recordingId != null) {
+      const trackIds = await listTrackIdsByRecordingId(recordingId)
+      if (trackIds.length === 1) {
+        const result = await listRecordingIdsByTrackId(trackIds[0])
+        if (result.length > 1) {
+          openUrl(`https://acoustid.org/track/${trackIds[0]}`)
+        }
+      }
+    } else {
+      clearInterval(intervalId)
     }
-  }
+  }, 300)
 }
 
-lookupAcoustIdByTrackId('47b1aca5-5ec9-41a4-93c7-ceadad334087')
+if (process.argv.length < 3) {
+  console.log(`Usage: ${process.argv[1]} <musicbrainz-release-id>`)
+  process.exit(1)
+}
+
+openAcoustIdWithMultipleRecordings(process.argv[2])
