c91f8a41266c4a6aa7ddb1a851612f72fb4a82ce
Josef Friedrich <josef@friedrich.rocks>
2023-07-22 17:14:19 +0200

    Get recording IDs by release

--- src/main.js
+++ src/main.js
@@ -2,6 +2,32 @@
 
 const URLSearchParams = require('url').URLSearchParams
 
+const MusicBrainzApi = require('musicbrainz-api').MusicBrainzApi
+
+const mbApi = new MusicBrainzApi({
+  appName: 'queryReleases',
+  appVersion: '0.1.0',
+  appContactInfo: 'josef@friedrich.rocks'
+})
+
+// release
+// https://musicbrainz.org/release/70d0009e-3b8d-4e03-ab41-2beb34a2c546
+
+async function getRecordingIdsByRelease (releaseId) {
+  const release = await mbApi.lookupRelease(releaseId, ['recordings'])
+  const recordingIds = []
+  for (const cd of release.media) {
+    for (const track of cd.tracks) {
+      recordingIds.push(track.recording.id)
+    }
+  }
+  return recordingIds
+}
+
+getRecordingIdsByRelease('70d0009e-3b8d-4e03-ab41-2beb34a2c546').then((recordingIds) => {
+  console.log(recordingIds)
+})
+
 // mbid: MusicBrainz Recording ID
 // 36c668ac-3cfb-44d9-a8e6-0a1d65a77a76
 
